622c0e22b4a111f56a3803a7f410aeb27c0c7386
Yonggang Luo <luoyonggang@gmail.com>
2025-12-30 21:10:46 +0800

    bootstraping

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,19 @@
-# MSYS2 building
-
-## Building msys2-runtime
-
-```bash
 pushd ./ports/msys2-runtime
+pacman -Syu base-devel --noconfirm
 updpkgsums
-makepkg --cleanbuild --syncdeps --force
+makepkg --cleanbuild --syncdeps --force --noconfirm
 tar xf msys2-runtime-devel-3.6.5.1-1-x86_64.pkg.tar.zst  -C /
 tar xf msys2-runtime-3.6.5.1-1-x86_64.pkg.tar.zst  -C /
 mv msys2-runtime-devel-3.6.5.1-1-x86_64.pkg.tar.zst ../../dist/msys2-runtime-devel-3.6.5.1-1-x86_64-stage0.pkg.tar.zst
 mv msys2-runtime-3.6.5.1-1-x86_64.pkg.tar.zst ../../dist/msys2-runtime-3.6.5.1-1-x86_64-stage0.pkg.tar.zst
-makepkg --cleanbuild --syncdeps --force
+makepkg --cleanbuild --syncdeps --force --noconfirm
 tar xf msys2-runtime-devel-3.6.5.1-1-x86_64.pkg.tar.zst  -C /
 tar xf msys2-runtime-3.6.5.1-1-x86_64.pkg.tar.zst  -C /
 mv msys2-runtime-devel-3.6.5.1-1-x86_64.pkg.tar.zst ../../dist
 mv msys2-runtime-3.6.5.1-1-x86_64.pkg.tar.zst ../../dist/
-```
 
-## Building crosstool-ng
+popd
 
-```bash
-pushd /c/work/xemu/MSYS2-packages/crosstool-ng/
-updpkgsums
-makepkg --cleanbuild --syncdeps --force --install --noconfirm
-```
+pushd ./ports/zstd
+makepkg --cleanbuild --syncdeps --force --noconfirm
+popd
