f35d5204b38a709e53ccb4396fbad2410de788d9
Yonggang Luo <luoyonggang@gmail.com>
2026-01-04 07:02:31 +0800

    ignore more

--- building.sh
+++ building.sh
@@ -1,5 +1,10 @@
+pushd ./ports/libiconv
+makepkg --cleanbuild --syncdeps --force --noconfirm
+mv bash-completion-2.17.0-2-any.pkg.tar.zst ../../dist
+popd
 
 pushd ./ports/bash-completion
 makepkg --cleanbuild --syncdeps --force --noconfirm
 mv bash-completion-2.17.0-2-any.pkg.tar.zst ../../dist
 popd
+
